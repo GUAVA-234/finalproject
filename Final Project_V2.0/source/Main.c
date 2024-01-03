6b9198fd08281ed4e6c4cf0d9c87282a35eb25ef
112360231 <t112360231@ntut.com.tw>
2024-01-03 14:27:45 +0800

    程式說明

--- Final Project_V2.0/source/Main.c
+++ Final Project_V2.0/source/Main.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-int bossMaxHealth;
+int bossMaxHealth;// Boss 最大血量
+
+typedef struct
+{
+	int health;// 玩家血量
+	int attack;// 玩家攻擊力
+	int heal;  // 玩家恢復量
+	int action_interval;// 玩家行動間隔
+	int next_action_turn;// 玩家下次行動回合
+} Player;
 
 typedef struct
 {
 	int health;
-	int attack;
-	int heal;
-	int action_interval;
-	int next_action_turn;
-} Player;
-
-typedef struct 
-{
-	int health;
 } Boss;
-
-void initializePlayer(Player *player, int health, int attack, int heal, int action_interval) 
+// 初始化玩家屬性
+void initializePlayer(Player *player, int health, int attack, int heal, int action_interval)
 {
 	player->health = health;
 	player->attack = attack;
@@ -26,15 +26,15 @@ void initializePlayer(Player *player, int health, int attack, int heal, int acti
 	player->action_interval = action_interval;
 	player->next_action_turn = 1;
 }
-
+// 初始化 Boss 屬性
 void initializeBoss(Boss *boss, int health)
 {
 	boss->health = health;
 }
 
-int getRandom(int min, int max) 
+int getRandom(int min, int max)
 {
-	return min + rand() % (max - min + 1);
+	return min + rand() % (max - min + 1);// 取得指定範圍內的隨機數
 }
 
 void displayHealthBar(int currentHealth, int maxHealth)
@@ -43,20 +43,20 @@ void displayHealthBar(int currentHealth, int maxHealth)
 	int healthUnits = (currentHealth * barWidth) / maxHealth;
 
 	printf("[");
-	for (int i = 0; i < barWidth; i++) 
+	for (int i = 0; i < barWidth; i++)
 	{
-		if (i < healthUnits) 
+		if (i < healthUnits)
 		{
 			printf("\033[37m█\033[37m");
 		}
-		else 
+		else
 		{
 			printf(" ");
 		}
 	}
 	printf("] %d/%d\n", currentHealth, maxHealth);
 }
-
+// 顯示玩家及 Boss 的血量條（紅色）
 void displayHealthBarRED(int currentHealth, int maxHealth)
 {
 	int barWidth = 20;
@@ -67,16 +67,16 @@ void displayHealthBarRED(int currentHealth, int maxHealth)
 	{
 		if (i < healthUnits)
 		{
-			printf("\033[31m█\033[37m");
+			printf("\033[31m█\033[37m");// 紅色顯示血量
 		}
 		else
 		{
 			printf(" ");
 		}
 	}
-	printf("] %d/%d\n", currentHealth, maxHealth);
+	printf("] %d/%d\n", currentHealth, maxHealth); //顯示當前血量及最大血量
 }
-
+// 顯示玩家的血量條（綠色）
 void displayHealthBarGREEN(int currentHealth, int maxHealth)
 {
 	int barWidth = 20;
@@ -87,28 +87,28 @@ void displayHealthBarGREEN(int currentHealth, int maxHealth)
 	{
 		if (i < healthUnits)
 		{
-			printf("\033[32m█\033[37m");
+			printf("\033[32m█\033[37m");// 綠色表示血量
 		}
 		else
 		{
 			printf(" ");
 		}
 	}
-	printf("] %d/%d\n", currentHealth, maxHealth);
+	printf("] %d/%d\n", currentHealth, maxHealth);// 顯示當前血量及最大血量
 }
-
-void performAction(Player *player, Boss *boss, int turn,int number)
+// 玩家執行攻擊或治療動作
+void performAction(Player *player, Boss *boss, int turn, int number)
 {
-	if (turn >= player->next_action_turn) 
+	if (turn >= player->next_action_turn)
 	{
-		printf("玩家 %d 行動 (1-攻擊, 2-治療): ",number);
+		printf("玩家 %d 行動 (1-攻擊, 2-治療): ", number);
 		int choice;
 		scanf("%d", &choice);
 
 		if (choice == 1)
 		{
 			int damage = getRandom(0, player->attack);
-			printf("玩家 %d 攻擊 %d 點傷害!\n",number, damage);
+			printf("玩家 %d 攻擊 %d 點傷害!\n", number, damage);
 			boss->health -= damage;
 			printf("Boss 血量: ");
 			displayHealthBarRED(boss->health, bossMaxHealth);// 更新Boss血量條
@@ -117,7 +117,7 @@ void performAction(Player *player, Boss *boss, int turn,int number)
 		else
 		{
 			int heal = getRandom(0, player->heal);
-			printf("玩家 %d 恢復 %d 點生命!\n",number ,heal);
+			printf("玩家 %d 恢復 %d 點生命!\n", number, heal);
 			player->health += heal;
 			if (player->health > 100) player->health = 100;
 			displayHealthBarGREEN(player->health, 100); // 更新玩家血量條
@@ -127,13 +127,13 @@ void performAction(Player *player, Boss *boss, int turn,int number)
 		player->next_action_turn += player->action_interval;
 	}
 }
-
-void bossAction(Player players[], int playerCount) 
+// Boss 執行攻擊動作
+void bossAction(Player players[], int playerCount)
 {
-	
+
 	int target = getRandom(0, playerCount - 1);
 	int damage = getRandom(10, 30);
-	if (players[target].health!=0)
+	if (players[target].health != 0)
 	{
 		printf("BOSS攻擊玩家 %d 造成 %d 點傷害!\n", target + 1, damage);
 		players[target].health -= damage;
@@ -146,37 +146,37 @@ void bossAction(Player players[], int playerCount)
 	}
 	else
 	{
-		return bossAction(players, playerCount);
+		return bossAction(players, playerCount);// 如果被攻擊的玩家已死亡，重新選擇目標玩家
 	}
 }
 
-int main() 
+int main()
 {
 	srand(time(NULL));
 
 	Player players[3];
 	Boss boss;
 	int difficulty;
-
+	// 遊戲標題及難度選擇
 	printf("   ==========================\n");
 	printf("   ∥                        ∥\n");
 	printf("   ∥        陽壽打怪        ∥\n");
 	printf("   ∥                        ∥\n");
 	printf("   ==========================\n");
 
-	
+
 	printf("請選擇難度 (1-簡單, 2-普通, 3-困難): ");
 	scanf("%d", &difficulty);
 	printf("\n\n");
 	bossMaxHealth = (difficulty == 1) ? 300 : (difficulty == 2) ? 500 : 1000;
-	initializeBoss(&boss, bossMaxHealth);
+	initializeBoss(&boss, bossMaxHealth);// 初始化 Boss 血量
 
 	initializePlayer(&players[0], 100, 10, 20, 1); // Player A
 	initializePlayer(&players[1], 100, 20, 40, 2); // Player B
 	initializePlayer(&players[2], 100, 30, 60, 3); // Player C
 
 	int turn = 1;
-	while (boss.health > 0) 
+	while (boss.health > 0)// 遊戲進行中
 	{
 		printf("回合 %d\n", turn);
 		printf("玩家 1 血量: ");
@@ -187,14 +187,14 @@ int main()
 		displayHealthBar(players[2].health, 100);
 		printf("Boss 血量:   ");
 		displayHealthBar(boss.health, bossMaxHealth);
-		
+
 		printf("\n");
 
-		for (int i = 0; i < 3; i++) 
+		for (int i = 0; i < 3; i++)
 		{
-			if (players[i].health > 0) 
+			if (players[i].health > 0)
 			{
-				performAction(&players[i], &boss, turn, i+1);
+				performAction(&players[i], &boss, turn, i + 1);
 				if (boss.health <= 0)
 				{
 					printf("BOSS死了 玩家勝利！\n");
@@ -204,9 +204,9 @@ int main()
 		}
 
 		bossAction(players, 3);
-		for (int i = 0; i < 3; i++) 
+		for (int i = 0; i < 3; i++)
 		{
-			if (players[i].health <= 0) 
+			if (players[i].health <= 0)
 			{
 				printf("玩家 %d 死了!\n", i + 1);
 			}
@@ -222,7 +222,7 @@ int main()
 			}
 		}
 
-		if (allPlayersDead) 
+		if (allPlayersDead)
 		{
 			printf("%d\n", allPlayersDead);
 			printf("所有玩家都死亡了! BOSS贏了!\n");
@@ -235,7 +235,7 @@ int main()
 		system("pause");
 		system("cls");
 	}
-	
+
 
 	return 0;
 }
